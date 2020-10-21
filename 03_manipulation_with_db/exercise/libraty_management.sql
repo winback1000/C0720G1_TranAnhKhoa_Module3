68e4ba1a7a0af780b566d9fe2bfcc92bf526c3bf
C0720G1-TranAnhKhoa <khoa.trananh2@gmail.com>
2020-10-21 16:27:33 +0700

    manupulation with DB

---
+++ 03_manipulation_with_db/exercise/libraty_management.sql
@@ -0,0 +1,32 @@
+create database library_management;
+use library_management;
+create table students(
+id int primary key,
+`name` varchar(30) not null,
+date_of_birth date,
+address varchar(100),
+email varchar(30),
+phone_num varchar(10),
+image varchar(100)
+);
+create table books(
+id int primary key,
+`name` varchar(100),
+catagory int
+);
+create table catagory(
+id int primary key,
+`name` varchar(15)
+);
+create table borrow_order(
+student_id int,
+book_id int,
+borrow_date date,
+return_date date
+);
+alter table books modify catagory int,
+add foreign key (catagory) references catagory(id);
+alter table borrow_order
+add foreign key (student_id) references students(id),
+add foreign key (book_id) references books(id),
+add primary key(student_id, book_id);
---
+++ 03_manipulation_with_db/practice/group_by_statement.sql
@@ -0,0 +1,22 @@
+SELECT * FROM classicmodels.orders;
+select status from orders 
+group by status;
+select status, COUNT(*) as 'So luong status'
+from orders
+group by status;
+select status, sum(quantityOrdered * priceEach) as amount
+from orders 
+inner join orderdetails on orders.ordernumber = orderdetails.ordernumber
+group by status;
+select orderNumber, sum(quantityOrdered * priceEach) as total
+from orderdetails
+group by orderNumber;
+select year(orderDate) as year, sum(quantityOrdered * priceEach) as total
+from orders 
+inner join orderdetails on orders.orderNumber = orderdetails.orderNumber
+where status = 'shipped'
+group by year
+having year > 2003;
+select orderNumber, `status`, comments from orders 
+where orders.`status` = 'Shipped'
+group by orderNumber;
