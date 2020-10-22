57d928145d4d4ae698c8b5aba4f7880d0db6d8ef
C0720G1-TranAnhKhoa <khoa.trananh2@gmail.com>
2020-10-22 15:38:39 +0700

    update 04. functions in mySQL

--- 04_functions_in_mySQL/exercise/functions_in_MySQL.sql
+++ 04_functions_in_mySQL/exercise/functions_in_MySQL.sql
@@ -15,5 +15,7 @@ insert into students (ten, tuoi, khoa_hoc, so_tien) values
 ('Huong',20,'TCNH', 500000),
 ('Huong',20,'TCNH', 200000);
 select * from students where ten = 'Huong';
-select sum(so_tien) from students where ten = 'Huong';
+select id, ten, khoa_hoc, sum(so_tien) as total from students where ten = 'Huong';
 select distinct ten as unique_name from students
+-- right join students
+-- on students.id = student.id
